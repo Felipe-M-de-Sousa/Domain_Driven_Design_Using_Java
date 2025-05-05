311c306eec7888cdaacda3723649f400ab458581
FelipeSousa <felipe.sousa.1106@gmail.com>
2025-05-05 10:23:40 -0300

    Add files via upload
    projeto da aula 05/05.

---
+++ Projeto12/src/br/com/fiap/main/Main.java
@@ -0,0 +1,50 @@
+package br.com.fiap.main;
+
+import br.com.fiap.bean.Geometria;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Geometria geo = new Geometria();
+        String auxiliar, escolha = "sim";
+        int opcao;
+        float lado, altura;
+        double raio;
+        while (escolha.equalsIgnoreCase("sim")) {
+            try {
+                auxiliar = JOptionPane.showInputDialog("Qual área deseja calcular? \n(1) Quadrado \n(2) Retângulo \n(3) Círculo");
+                opcao = Integer.parseInt(auxiliar);
+                switch (opcao) {
+                    case 1:
+                        auxiliar = JOptionPane.showInputDialog("Digite valor do lado");
+                        lado = Float.parseFloat(auxiliar);
+                        geo.calcularArea(lado);
+                        break;
+                    case 2:
+                        auxiliar = JOptionPane.showInputDialog("Digite valor do lado");
+                        lado = Float.parseFloat(auxiliar);
+                        auxiliar = JOptionPane.showInputDialog("Digite valor do altura");
+                        altura = Float.parseFloat(auxiliar);
+                        geo.calcularArea(lado, altura);
+                        break;
+                    case 3:
+                        auxiliar = JOptionPane.showInputDialog("Digite valor do lado");
+                        lado = Float.parseFloat(auxiliar);
+                        auxiliar = JOptionPane.showInputDialog("Digite valor do altura");
+                        altura = Float.parseFloat(auxiliar);
+                        auxiliar = JOptionPane.showInputDialog("Digite valor do raio");
+                        raio = Double.parseDouble(auxiliar);
+                        geo.calcularArea(raio);
+                        break;
+                    default:
+                        throw new Exception("Escolha incorreta!");
+                }
+                escolha = JOptionPane.showInputDialog("Deseja continuar?");
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Fim de Programa. Volte sempre!");
+    }
+}
